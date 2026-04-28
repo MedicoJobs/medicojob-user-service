5888b8fe0044e12aebc2aa4e1540f38e16885708
PaviTechDevloper <pavikutty0609@gmail.com>
2026-04-28 10:54:56 +0530

    fix(user serive): update 3

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use('/auth', authRoutes);
 
 const MONGO_URI = process.env.MONGO_URI_USER || process.env.MONGO_URI;
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
